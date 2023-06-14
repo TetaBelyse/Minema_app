6589b867b6d9fb05c53d17a86b86d5e74c34472b
Teta Belyse <tetabelyse2@gmail.com>
2023-06-14 16:36:38 +0200

    Refactor navbar

--- src/layout/BaseLayout.jsx
+++ src/layout/BaseLayout.jsx
@@ -6,7 +6,7 @@ const MenuItem = ({ title, path }) => {
     const location = useLocation()
 
     return (
-        <div onClick={() => navidate(path)} className={`cursor-pointer hover:underline underline-offset-4 ${path == location.pathname ? 'underline' : ''}`}>{title}</div>
+        <div onClick={() => navidate(path)} className={`cursor-pointer decoration-4 text-sm decoration-amber-400 hover:underline underline-offset-4 ${path == location.pathname ? 'underline' : ''}`}>{title}</div>
     )
 }
 
@@ -16,16 +16,16 @@ const BaseLayout = ({ children }) => {
 
     return (
         <div className="w-screen min-h-screen bg-amber-500">
-            <nav className="w-screen bg-amber-600 sticky top-0 text-gray-200 flex justify-between items-center px-2 md:px-24 font-semibold text-sm py-2">
+            <nav className="w-screen shadow-lg bg-amber-600 sticky top-0 text-gray-200 flex justify-between items-center px-2 md:px-24 font-semibold text-sm py-2">
                 <div onClick={() => navidate('/')} className="cursor-pointer">Logo</div>
                 <div className="flex gap-10">
-                    <div className="flex gap-6 items-center uppercase">
-                        <MenuItem title={'home'} path={'/'} />
-                        <MenuItem title={'page1'} path={'/page1'} />
-                        <MenuItem title={'page2'} path={'/page2'} />
-                        <MenuItem title={'page3'} path={'/page3'} />
+                    <div className="flex gap-6  items-center uppercase">
+                        <MenuItem title={'Home'} path={'/'} />
+                        <MenuItem title={'Page1'} path={'/page1'} />
+                        <MenuItem title={'Page2'} path={'/page2'} />
+                        <MenuItem title={'Page3'} path={'/page3'} />
                     </div>
-                    <div className="bg-white rounded-full p-2 px-8 font-bold text-xs text-amber-700 cursor-pointer">Login</div>
+                    <div className="bg-white rounded-full p-2 px-8 font-semibold text-sm text-amber-700 cursor-pointer">Login</div>
                 </div>
             </nav>
             <div className="w-full text-center text-white">{children}</div>
