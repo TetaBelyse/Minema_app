0b1231758dc426b79b0e334f96a5d4e52c01c247
umupapa <fatirutagengwa1@gmail.com>
2023-06-27 11:46:53 +0200

    adding signin page

--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import sidebarItems from "../utils/sidebarItems";
-import SignIn from "../Authentications/SignIn"
-
+import SignIn from "../Authentications/SignIn";
+import Home from "../Authentications/Home";
 
 const AllRoutes = () => {
   return (
@@ -9,9 +9,8 @@ const AllRoutes = () => {
       {sidebarItems.map((item) => (
         <Route key={item.id} path={item.path} element={item.element} />
       ))}
-      <Route
-        path='/signIn'
-        element={< SignIn />} />
+      <Route path="/signIn" element={<SignIn />} />
+      <Route path="/" element={<Home />} />
     </Routes>
   );
 };
