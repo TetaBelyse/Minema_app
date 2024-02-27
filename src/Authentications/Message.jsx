af6b323fd36fd8d5f2b4f0bf5812986e2b66c625
Francois Rutagengwa <frut@esrirw.com>
2024-02-27 10:07:45 +0200

    my changes pages

---
+++ src/Authentications/Message.jsx
@@ -0,0 +1,9 @@
+const Message = () => {
+  return (
+    <div className="flex items-center justify-center mt-11 ">
+      <p className="text-4xl font-extrabold">UNDER CONSTRUCTION</p>
+    </div>
+  );
+};
+
+export default Message;
